c55783558d99f48274e6a525108f3fc89569460a
Bendik August Nesbø <bendik.nesbo@gmail.com>
2017-02-12 12:06:26 +0100

    Added option (default false) to set Skype's Audio-Out device.

--- AudioDefaultDeviceSwitcher/Program.cs
+++ AudioDefaultDeviceSwitcher/Program.cs
@@ -12,6 +12,7 @@ namespace AudioDefaultDeviceSwitcher {
         private const string IniFileName = "Audio.ini";
         private const string DevicesIniKey = "Devices";
         private const string AlsoSetCommunicationsIniKey = "AlsoSetCommunications";
+        private const string FixSkypeIniKey = "FixSkype";
         private readonly string _iniPath = AppDomain.CurrentDomain.BaseDirectory + IniFileName;
         private readonly string _logPath = AppDomain.CurrentDomain.BaseDirectory + "Audio.log";
 
@@ -35,15 +36,28 @@ namespace AudioDefaultDeviceSwitcher {
             CoreAudioDevice nextDevice = currentIndex == relevantDevicesArr.Length - 1
                 ? relevantDevicesArr.First()
                 : relevantDevicesArr[currentIndex + 1];
+
             nextDevice.SetAsDefault();
             var alsoCommunications = ini.Val(AlsoSetCommunicationsIniKey, true);
             if (alsoCommunications)
                 nextDevice.SetAsDefaultCommunications();
+
+            var fixSkype = ini.Val(FixSkypeIniKey, false);
+            if (fixSkype) {
+                try {
+                    new SkypeFixer(nextDevice).SetAudioOut();
+                } catch (Exception ex) {
+                    CreateAndWriteToFile(_logPath, "Fixing skype failed. Exception: " + ex);
+                    Environment.Exit(0);
+                }
+            }
         }
 
         private void Initialize() {
             if (!File.Exists(_iniPath)) {
-                var defaultIni = DevicesIniKey + "=A,B,C" + Environment.NewLine + AlsoSetCommunicationsIniKey + "true";
+                var defaultIni = DevicesIniKey + "=A,B,C" + Environment.NewLine
+                    + AlsoSetCommunicationsIniKey + "=true" + Environment.NewLine
+                    + FixSkypeIniKey + "=false";
                 CreateAndWriteToFile(_iniPath, defaultIni);
                 CreateAndWriteToFile(_logPath, string.Format("A {0} has been created for you, fill it with the playback devices you want to toggle between.\r\nPath to the .ini: {1}", IniFileName, _iniPath));
                 Environment.Exit(0);
---
+++ AudioDefaultDeviceSwitcher/SkypeFixer.cs
@@ -0,0 +1,23 @@
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using SKYPE4COMLib;
+
+namespace AudioDefaultDeviceSwitcher {
+    public class SkypeFixer {
+        private readonly CoreAudioDevice _device;
+        private static Skype _skype;
+        public SkypeFixer(CoreAudioDevice device) {
+            _device = device;
+            _skype = new Skype();
+        }
+
+        public void Fix() {
+            if (IsLoggedIn()) {
+                _skype.Settings.AudioOut = _device.FullName;
+            }
+        }
+
+        private bool IsLoggedIn() {
+            return _skype.Client.IsRunning && _skype.CurrentUserStatus != TUserStatus.cusLoggedOut;
+        }
+    }
+}
